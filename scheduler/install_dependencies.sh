6bd92b0396e5a58acd03b012747682a10e469ef4
Ryan <mhogar98@gmail.com>
2019-12-06 11:59:05 -0500

    Updated install script to install rapidjson

--- scheduler/install_dependencies.sh
+++ scheduler/install_dependencies.sh
@@ -1,5 +1,13 @@
-# TODO: add install script(s) for rapidJSON
+echo "Installing rapidJSON..."
+cd include
+echo "Downloading soruse and extracting..."
+wget https://github.com/Tencent/rapidjson/archive/v1.1.0.tar.gz -O rapidjson.tar.gz && tar -zxvf rapidjson.tar.gz
+echo "Copying required files..."
+pwd && cp -r rapidjson-1.1.0/include/rapidjson/* ./rapidjson
+echo "Cleaning up..."
+rm -rf rapidjson.tar.gz rapidjson-1.1.0/
+echo "Done."
 
-# catch2 (unit testing)
 echo "Installing catch2..."
 curl https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp --output include/catch2/catch.hpp
+echo "Done."
